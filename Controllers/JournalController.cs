a0592fd66c32ad799b3f8df537125559e458f4a5
goolsbysarah <sgoolsby@nevada.unr.edu>
2022-03-09 17:32:29 -0800

    with andrew, fixed issue

--- Controllers/JournalController.cs
+++ Controllers/JournalController.cs
@@ -61,14 +61,16 @@ namespace _2122_Senior_Project_06.Controllers
         }
         
         // CREATE
-        [HttpPost("Create")]
-        public IActionResult Create([FromBody] JournalEntry potentialJournal)
+        [HttpPost("Create/{id}")]
+        public IActionResult Create(string id, [FromBody] JournalEntry potentialJournal)
         {
+            
+            Console.WriteLine(string.Format("Test ID: {0}", id));
             try
             {
                 potentialJournal.LastUpdated = DateTime.Now;
                 JournalEntry newEntry = new JournalEntry(potentialJournal.Title, potentialJournal.Body, 
-                                                        potentialJournal.UserID, potentialJournal.LastUpdated);
+                                                        id, potentialJournal.LastUpdated);
                 JournalsDataTable.AddNewEntry(newEntry);
                 return Ok();
             }
