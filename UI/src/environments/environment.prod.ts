f20686dd0c4a54959b945d176082464838d1639a
HugoMazari <hugojrh@nevada.unr.edu>
2022-04-29 00:42:49 -0700

    changed https call to http and added routes.json

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://proud-grass-08e024b0f.1.azurestaticapps.net:5000',
+  api: 'http://proud-grass-08e024b0f.1.azurestaticapps.net:5000',
   //we will add azure url here to this page
 };
